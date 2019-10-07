fbc2504fb7542a437f03beaab27e0a547fa7cc9f
egconley <31393715+egconley@users.noreply.github.com>
2019-10-07 15:46:55 -0700

    got cookies purchased per hour to display on browser for shop 1 and 2

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var parentEl = document.getElementById('parentElement');
 parentEl.textContent = 'attempting to contact html';
 
-var child = document.createElement('h1');
+var child = document.createElement('h2');
 child.textContent = 'proof of life';
 parentEl.appendChild(child);
 
 var downtownShop = {
   shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
-  hourTotals: [],
+  hourCustomerVisits: [],
+  min: 10,
+  max: 25,
+  avgCookiesPerCustVisit: 2,
+  cookiesPurchased: [],
   locationName: 'Pike Place Market',
   randoNumberGen: function(min, max) {
     var min = Math.ceil(min);
@@ -17,10 +21,36 @@ var downtownShop = {
   render: function() {
     for (var i = 0; i < this.shopHours.length; i++) {
       var childEl = document.createElement('li');
-      this.hourTotals.push(this.randoNumberGen(5, 9));
-      childEl.textContent = `Shop Hours: ${this.shopHours[i]} ~ Total Customers: ${this.hourTotals[i]}`;
+      this.hourCustomerVisits.push(this.randoNumberGen(this.min, this.max));
+      this.cookiesPurchased.push(this.hourCustomerVisits[i]*this.avgCookiesPerCustVisit)
+      childEl.textContent = `Shop Hours: ${this.shopHours[i]} ~ Total Customers: ${this.hourCustomerVisits[i]} ~ Cookies Purchased: ${this.cookiesPurchased[i]}`;
       parentEl.appendChild(childEl);
     }
   }
 }
 downtownShop.render();
+
+var greenwoodShop = {
+  shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  hourCustomerVisits: [],
+  min: 5,
+  max: 15,
+  avgCookiesPerCustVisit: 3,
+  cookiesPurchased: [],
+  locationName: '85th and Greenwood',
+  randoNumberGen: function(min, max) {
+    var min = Math.ceil(min);
+    var max = Math.floor(max);
+    return Math.floor(Math.random() * (max-min)) + min;
+  },
+  render: function() {
+    for (var i = 0; i < this.shopHours.length; i++) {
+      var childEl = document.createElement('li');
+      this.hourCustomerVisits.push(this.randoNumberGen(this.min, this.max));
+      this.cookiesPurchased.push(this.hourCustomerVisits[i]*this.avgCookiesPerCustVisit)
+      childEl.textContent = `Shop Hours: ${this.shopHours[i]} ~ Total Customers: ${this.hourCustomerVisits[i]} ~ Cookies Purchased: ${this.cookiesPurchased[i]}`;
+      parentEl.appendChild(childEl);
+    }
+  }
+}
+greenwoodShop.render();
