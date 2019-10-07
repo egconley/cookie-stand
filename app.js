dff3381e530703a057d6d6ce072a8f0d0d76bbfd
egconley <31393715+egconley@users.noreply.github.com>
2019-10-07 16:40:20 -0700

    got shop four up and running

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ var child = document.createElement('h1');
 child.textContent = 'Salmon Cookies';
 parentEl.appendChild(child);
 
-var downtownShop = {
+var seattle = {
   shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   hourCustomerVisits: [],
-  min: 10,
-  max: 25,
-  avgCookiesPerCustVisit: 2,
+  min: 23,
+  max: 65,
+  avgCookiesPerCustVisit: 6.3,
   cookiesPurchased: [],
   totCookiesSold: [0],
-  locationName: 'Pike Place Market',
+  locationName: 'Seattle',
   randoNumberGen: function(min, max) {
     var min = Math.ceil(min);
     var max = Math.floor(max);
@@ -40,21 +40,21 @@ var downtownShop = {
     parentEl.appendChild(childEl2);
   }
 }
-downtownShop.displayStoreName();
-downtownShop.render();
-downtownShop.totalSold();
+seattle.displayStoreName();
+seattle.render();
+seattle.totalSold();
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var greenwoodShop = {
+var tokyo = {
   shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   hourCustomerVisits: [],
-  min: 5,
-  max: 15,
-  avgCookiesPerCustVisit: 3,
+  min: 3,
+  max: 24,
+  avgCookiesPerCustVisit: 1.2,
   cookiesPurchased: [],
   totCookiesSold: [0],
-  locationName: '85th and Greenwood',
+  locationName: 'Tokyo',
   randoNumberGen: function(min, max) {
     var min = Math.ceil(min);
     var max = Math.floor(max);
@@ -81,21 +81,62 @@ var greenwoodShop = {
     parentEl.appendChild(childEl2);
   }
 }
-greenwoodShop.displayStoreName();
-greenwoodShop.render();
-greenwoodShop.totalSold();
+tokyo.displayStoreName();
+tokyo.render();
+tokyo.totalSold();
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var uDistrictShop = {
+var dubai = {
+  shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  hourCustomerVisits: [],
+  min: 11,
+  max: 38,
+  avgCookiesPerCustVisit: 3.7,
+  cookiesPurchased: [],
+  totCookiesSold: [0],
+  locationName: 'Dubai',
+  randoNumberGen: function(min, max) {
+    var min = Math.ceil(min);
+    var max = Math.floor(max);
+    return Math.floor(Math.random() * (max-min)) + min;
+  },
+  displayStoreName: function() {
+    var childEl0 = document.createElement('h2');
+    childEl0.textContent = `${this.locationName}`;
+    parentEl.appendChild(childEl0);
+  },
+  render: function() {
+    for (var i = 0; i < this.shopHours.length; i++) {
+      var childEl = document.createElement('li');
+      this.hourCustomerVisits.push(this.randoNumberGen(this.min, this.max));
+      this.cookiesPurchased.push(this.hourCustomerVisits[i]*this.avgCookiesPerCustVisit);
+      this.totCookiesSold.push(this.totCookiesSold[i]+this.cookiesPurchased[i]);
+      childEl.textContent = `Shop Hours: ${this.shopHours[i]} ~ Total Customers: ${this.hourCustomerVisits[i]} ~ Cookies Purchased: ${this.cookiesPurchased[i]}`;
+      parentEl.appendChild(childEl);
+    }
+  },
+  totalSold: function() {
+    var childEl2 = document.createElement('li');
+    childEl2.textContent = `Total Cookies Sold: ${this.totCookiesSold[this.shopHours.length -1]}`;
+    parentEl.appendChild(childEl2);
+  }
+}
+dubai.displayStoreName();
+dubai.render();
+dubai.totalSold();
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var paris = {
   shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   hourCustomerVisits: [],
   min: 20,
-  max: 50,
-  avgCookiesPerCustVisit: 3,
+  max: 38,
+  avgCookiesPerCustVisit: 2.3,
   cookiesPurchased: [],
   totCookiesSold: [0],
-  locationName: 'University District',
+  locationName: 'Paris',
   randoNumberGen: function(min, max) {
     var min = Math.ceil(min);
     var max = Math.floor(max);
@@ -122,9 +163,6 @@ var uDistrictShop = {
     parentEl.appendChild(childEl2);
   }
 }
-uDistrictShop.displayStoreName();
-uDistrictShop.render();
-uDistrictShop.totalSold();
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+paris.displayStoreName();
+paris.render();
+paris.totalSold();
