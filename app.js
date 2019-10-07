646d39057e96923f969099b407f98b1f648bfc91
egconley <31393715+egconley@users.noreply.github.com>
2019-10-07 14:59:02 -0700

    got first location object literal with random number generator up and running

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ var child = document.createElement('h1');
 child.textContent = 'proof of life';
 parentEl.appendChild(child);
 
-var salmonCookieShop = {
+var downtownShop = {
   shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
-  hourTotals: [12, 3, 4, 56, 34, 12, 23, 43, 56, 73, 21, 44, 55, 24, 15],
+  hourTotals: [],
   locationName: 'Pike Place Market',
   render: function() {
     for (var i = 0; i < this.shopHours.length; i++) {
       var childEl = document.createElement('li');
       this.shopHours[i];
-      childEl.textContent = `Shop Hours: ${this.shopHours[i]} ~ Total Customers: ${this.hourTotals[i]}`;
+      this.hourTotals[i] = function(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max-min)) + min;
+      }
+      childEl.textContent = `Shop Hours: ${this.shopHours[i]} ~ Total Customers: ${this.hourTotals[i](5, 9)}`;
       parentEl.appendChild(childEl);
     }
   }
